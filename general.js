d6f1080a2db2f9123127c8c048d80baa1d23a954
Fernando Dugos <fddugos@gmail.com>
2023-05-30 20:10:02 -0300

    Finalización de ajustes en clases, variables y secciones para la optimización del código de toda la página

--- general.js
+++ general.js
@@ -2,22 +2,22 @@ AOS.init({
     duration: 1000
 });
 
-const iconoMenu = document.querySelector("#icono-menu"),
+const botonMenu = document.querySelector("#boton-menu"),
       menu = document.querySelector("#menu");
 
-iconoMenu.addEventListener("click", (e) =>{
+botonMenu.addEventListener("click", (e) =>{
     menu.classList.toggle("menu-activo");
-    iconoMenu.classList.toggle("boton-activo");
+    botonMenu.classList.toggle("boton-activo");
 
-    if (iconoMenu.classList.contains("boton-activo")) {
-        iconoMenu.style.transform = "rotate(180deg)";
-        iconoMenu.style.color = "var(--naranja)";
+    if (botonMenu.classList.contains("boton-activo")) {
+        document.querySelector("#icono-menu").style.transform = "rotate(180deg)";
+        document.querySelector("#icono-menu").style.color = "var(--naranja)";
         document.body.style.overflow="hidden";
         document.querySelector(".fondo-negro").style.zIndex="1";
         document.querySelector(".fondo-negro").style.opacity=".75";
       } else {
-        iconoMenu.style.transform = "rotate(0deg)";
-        iconoMenu.style.color = "var(--azul)";
+        document.querySelector("#icono-menu").style.transform = "rotate(0deg)";
+        document.querySelector("#icono-menu").style.color = "var(--azul)";
         document.body.style.overflow="auto";
         document.querySelector(".fondo-negro").style.zIndex="-1";
         document.querySelector(".fondo-negro").style.opacity="0";
